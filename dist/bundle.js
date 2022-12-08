9110ef3efae0520a8fd72522fdff32bf9067bca4
Nguyễn Đường <nguyenduong@Nguyens-MacBook-Pro.local>
2022-12-08 18:54:44 +0700

    init typescript

---
+++ dist/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log('test ts');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ub3RpZmljYXRpb24tcGFnZS1tYWluLy4vc3JjL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCd0ZXN0IHRzJyk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { LocalDataSource } from "./data/localDataSource";
+import { DomDisplay } from "./domDisplay";
+
+let ds = new LocalDataSource();
+
+async function displayData(): Promise<HTMLElement> {
+    let display = new DomDisplay();
+    display.props = {
+        products: await ds.getProducts("name"),
+        order: ds.order
+    }
+    return display.getContent();
+    // let ds = new LocalDataSource();
+    // let allProducts = await ds.getProducts("name");
+    // let categories = await ds.getCategories();
+    // let chessProducts = await ds.getProducts("name", "Chess");
+
+    // let result = "";
+
+    // allProducts.forEach(p => result += `Product: ${p.name}, ${p.category}\n`);
+    // categories.forEach(c => result += (`Category: ${c}\n`));
+    // chessProducts.forEach(p => ds.order.addProduct(p, 1));
+    // result += `Order total: $${ds.order.total.toFixed(2)}`;
+    // return result;
+}
+
+// displayData().then(res => console.log(res));
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        displayData().then(elem => {
+            let rootElement = document.getElementById("app");
+            rootElement.innerHTML = "";
+            rootElement.appendChild((elem));
+        })
+    }
+}
